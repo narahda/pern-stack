1bcb4f49871609d37f3e2005d217cecfe0d423ed
narahda <limnarahda@gmail.com>
2021-10-02 23:52:21 +0800

    ffmpeg
    added ffmpeg and other related codes for video streaming

---
+++ server/camera/cleaner.js
@@ -0,0 +1,9 @@
+const findRemoveSync = require('find-remove')
+
+setInterval(() => {
+    var result = findRemoveSync('./videos/ipcam', { age: { seconds: 30 }, extensions: '.ts' });
+    console.log(result);
+}, 5000);
+
+//ok do i really need this bc this doesnt seem to be coded right
+//or smthg along the way isnt coded right :(
