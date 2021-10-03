dfb57bf53d7737838faf6c39dd487e77a11801dc
narahda <limnarahda@gmail.com>
2021-10-03 18:53:31 +0800

    database
    added files to for user database creation

---
+++ server/app/controllers/usercontrol.js
@@ -0,0 +1,4 @@
+const db = require("../models");
+const Table = db.table;
+const Op = db.Sequelize.Op;
+
--- server/app/models/users.js
+++ server/app/models/users.js
@@ -8,5 +8,5 @@ module.exports = (sequelize, Sequelize) => {
       },
     });
   
-    return Table;
+    return User;
   };
