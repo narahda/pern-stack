b59aeb02be9b0083daec5b98fcd4c2973efc7675
narahda <limnarahda@gmail.com>
2021-10-17 18:31:05 +0800

    redo react side

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
-import React, {Component, useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import './App.css';
-import Navi from './navigation/navbar';
-import InfoCard from './cards/navcard';
 
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Navi/>
-        <InfoCard />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
